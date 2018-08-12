d491ef9facb3caa1f0614e091fcbac73292ac324
muktamittal <38948625+muktamittal@users.noreply.github.com>
2018-08-12 19:11:50 +0530

    6 png files for each question and single R code file

---
+++ prj2plot1.R
@@ -0,0 +1,108 @@
+## This first line will likely take a few seconds. Be patient!
+if(!exists("NEI")){
+    NEI <- readRDS("summarySCC_PM25.rds")
+}
+if(!exists("SCC")){
+    SCC <- readRDS("Source_Classification_Code.rds")
+}
+# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+# Using the base plotting system, make a plot showing the total PM2.5 emission from all sources 
+# for each of the years 1999, 2002, 2005, and 2008.
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, NEI, sum)
+
+png('prj2plot1.png')
+barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' emissions at various years'))
+dev.off()
+
+
+# Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? 
+# Use the base plotting system to make a plot answering this question.
+subsetNEI  <- NEI[NEI$fips=="24510", ]
+aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
+png('prj2plot2.png')
+barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' in the Baltimore City, MD emissions at various years'))
+dev.off()
+
+
+# Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, 
+# which of these four sources have seen decreases in emissions from 1999 2008 for Baltimore City? 
+# Which have seen increases in emissions from 1999 2008? 
+# Use the ggplot2 plotting system to make a plot answer this question.
+# 24510 is Baltimore
+library(ggplot2)
+subsetNEI  <- NEI[NEI$fips=="24510", ]
+
+aggregatedTotalByYearAndType <- aggregate(Emissions ~ year + type, subsetNEI, sum)
+
+png("prj2plot3.png", width=640, height=480)
+g <- ggplot(aggregatedTotalByYearAndType, aes(year, Emissions, color = type))
+g <- g + geom_line() +
+    xlab("year") +
+    ylab(expression('Total PM'[2.5]*" Emissions")) +
+    ggtitle('Total Emissions in Baltimore City, Maryland (fips == "24510") from 1999 to 2008')
+print(g)
+dev.off()
+
+
+
+
+
+# Across the United States, how have emissions from coal combustion-related sources changed from 1999-2008?
+# merge the two data sets 
+if(!exists("NEISCC")){
+    NEISCC <- merge(NEI, SCC, by="SCC")
+}
+
+# fetch all NEIxSCC records with Short.Name (SCC) Coal
+coalMatches  <- grepl("coal", NEISCC$Short.Name, ignore.case=TRUE)
+subsetNEISCC <- NEISCC[coalMatches, ]
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEISCC, sum)
+
+png("prj2plot4.png", width=640, height=480)
+g <- ggplot(aggregatedTotalByYear, aes(factor(year), Emissions))
+g <- g + geom_bar(stat="identity") +
+    xlab("year") +
+    ylab(expression('Total PM'[2.5]*" Emissions")) +
+    ggtitle('Total Emissions from coal sources from 1999 to 2008')
+print(g)
+dev.off()
+
+
+# How have emissions from motor vehicle sources changed from 1999-2008 in Baltimore City?
+# 24510 is Baltimore
+# Searching for ON-ROAD type in NEI
+subsetNEI <- NEI[NEI$fips=="24510" & NEI$type=="ON-ROAD",  ]
+aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
+
+png("prj2plot5.png", width=840, height=480)
+g <- ggplot(aggregatedTotalByYear, aes(factor(year), Emissions))
+g <- g + geom_bar(stat="identity") +
+    xlab("year") +
+    ylab(expression('Total PM'[2.5]*" Emissions")) +
+    ggtitle('Total Emissions from motor vehicle (type = ON-ROAD) in Baltimore City, Maryland (fips = "24510") from 1999 to 2008')
+print(g)
+dev.off()
+
+
+# Compare emissions from motor vehicle sources in Baltimore City with emissions from motor 
+# vehicle sources in Los Angeles County, California (fips == "06037"). 
+# Which city has seen greater changes over time in motor vehicle emissions?
+# 24510 is Baltimore, 06037 is LA CA
+# Searching for ON-ROAD type in NEI
+subsetNEI <- NEI[(NEI$fips=="24510"|NEI$fips=="06037") & NEI$type=="ON-ROAD",  ]
+
+aggregatedTotalByYearAndFips <- aggregate(Emissions ~ year + fips, subsetNEI, sum)
+aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="24510"] <- "Baltimore, MD"
+aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="06037"] <- "Los Angeles, CA"
+
+png("prj2plot6.png", width=1040, height=480)
+g <- ggplot(aggregatedTotalByYearAndFips, aes(factor(year), Emissions))
+g <- g + facet_grid(. ~ fips)
+g <- g + geom_bar(stat="identity")  +
+    xlab("year") +
+    ylab(expression('Total PM'[2.5]*" Emissions")) +
+    ggtitle('Total Emissions from motor vehicle (type=ON-ROAD) in Baltimore City, MD (fips = "24510") vs Los Angeles, CA (fips = "06037")  1999-2008')
+print(g)
+dev.off()
